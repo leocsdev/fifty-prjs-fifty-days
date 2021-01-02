371eb60d8ba866009b9b2e9e3a3e6a17f52807d3
Leo <leocsdev@gmail.com>
2021-01-03 02:10:16 +0800

    Added effects.

--- prj01-expanding-cards/script.js
+++ prj01-expanding-cards/script.js
@@ -1,1 +1,21 @@
-// alert('Hello from script.js');
+// select all panels
+const panels = document.querySelectorAll('.panel');
+
+// loop through all panels and add event listener for click event
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    // remove active classes to all
+    removeActiveClasses();
+    
+    // then add active class to the panel that is clicked
+    panel.classList.add('active');
+  });
+});
+
+
+function removeActiveClasses() {
+  // loop through all panels
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
