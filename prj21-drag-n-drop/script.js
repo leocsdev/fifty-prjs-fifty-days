8f66f1936603764ca53cb8e9179884acca3e600e
Leo <leocsdev@gmail.com>
2021-01-03 11:23:45 +0800

    added listeners and test drag drop events via console.

--- prj21-drag-n-drop/script.js
+++ prj21-drag-n-drop/script.js
@@ -1,1 +1,38 @@
 // alert('Hello from script.js');
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for(const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+  console.log('drag Start');
+}
+
+function dragEnd() {
+  console.log('drag End');
+}
+
+function dragOver() {
+  console.log('drag Over');
+}
+
+function dragEnter() {
+  console.log('drag Enter');
+}
+
+function dragLeave() {
+  console.log('drag Leave');
+}
+
+function dragDrop() {
+  console.log('drag Drop');
+}
